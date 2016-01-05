95df8d0cb5fa2b6b2e7eb7781d3dd7c3e2a61dcd
allen <allencoder@126.com>
2016-01-05 21:38:05 +0800

    增加测试页面

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 2. 支持手势显示当前值
 3. 支持当前的区域颜色渐变
 4. ![演示效果](http://img.blog.csdn.net/20151222175518831)
+5.如果ListView中的单个Item的view中存在checkbox，button等view，会导致ListView.setOnItemClickListener无效，
+  事件会被子View捕获到，ListView无法捕获处理该事件.
+  解决方法：
+  在checkbox、button对应的view处加
+  [html] 
+  ```xml
+  android:focusable="false"  
+  android:clickable="false"  
+  android:focusableInTouchMode="false"  
+  ```
--- app/src/main/java/com/allen/androiddevcoder/activity/ActivityList.java
+++ app/src/main/java/com/allen/androiddevcoder/activity/ActivityList.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -19,6 +20,7 @@ public class ActivityList extends AppCompatActivity {
     private Toolbar mToolbarToolbar;
     private FloatingActionButton mFabFloatingActionButton;
     private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,22 +31,41 @@ public class ActivityList extends AppCompatActivity {
         listView.setAdapter(new BaseAdapter());
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-               CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkBox);
-                TextView textView = (TextView) view.findViewById(R.id.textView);
-                if (checkBox.isChecked()){
-                    checkBox.setChecked(false);
-                    textView.setText("我被取消了");
-                }else {
-                    checkBox.setChecked(true);
-                    textView.setText("我被选中了");
-                }
-
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id) {
+                final CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkBox);
+                final TextView textView = (TextView) view.findViewById(R.id.textView);
+                final CheckBox checkBox1 = (CheckBox) view.findViewById(R.id.checkBox1);
+                final TextView textView1 = (TextView) view.findViewById(R.id.textView1);
+               checkBox.setOnCheckedChangeListener(parent.getOnItemSelectedListener());
+                view.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View v, MotionEvent event) {
+                        if (event.getX()<view.getWidth()/2){
+                            if (checkBox.isChecked()) {
+                                checkBox.setChecked(false);
+                                textView.setText("我被第一个取消了");
+                            } else {
+                                checkBox.setChecked(true);
+                                textView.setText("我被第一个选中了");
+                            }
+                        }else {
+                            if (checkBox1.isChecked()) {
+                                checkBox.setChecked(false);
+                                textView1.setText("我被第二个取消了");
+                            } else {
+                                checkBox1.setChecked(true);
+                                textView1.setText("我被第二个选中了");
+                            }
+                        }
+                        return false;
+                    }
+                });
             }
         });
 
     }
-    class  BaseAdapter extends android.widget.BaseAdapter{
+
+    class BaseAdapter extends android.widget.BaseAdapter {
 
         @Override
         public int getCount() {
@@ -70,13 +91,15 @@ public class ActivityList extends AppCompatActivity {
                 view.setTag(holder);
             } else {
                 holder = (ViewHolder) view.getTag();
-            }
+            }//            holder.checkBox.setOnCheckedChangeListener(new CheckedListener());
+
+//            holder.checkBox1.setOnCheckedChangeListener(new CheckedListener());
             return view;
         }
 
 
         class ViewHolder {
-            private CheckBox checkBox;
+            private CheckBox checkBox,checkBox1;
 
 
             ViewHolder(View view) {
@@ -84,8 +107,15 @@ public class ActivityList extends AppCompatActivity {
             }
 
             private void initViews(View root) {
-               checkBox = (CheckBox) root.findViewById(R.id.checkBox);
+                checkBox = (CheckBox) root.findViewById(R.id.checkBox);
+                checkBox1 = (CheckBox) root.findViewById(R.id.checkBox1);
             }
         }
     }
+//    class CheckedListener implements CompoundButton.OnCheckedChangeListener {
+//        @Override
+//        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//            System.out.println("compoundButton.getTag()" + compoundButton.getTag() + "b" + b);
+//        }
+//    }
 }

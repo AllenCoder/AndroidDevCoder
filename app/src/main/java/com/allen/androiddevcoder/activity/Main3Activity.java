58947a6821e5f93560eeccd83cc09f751073071f
allen <allencoder@126.com>
2016-03-01 10:55:47 +0800

    提交

---
+++ app/src/main/java/com/allen/androiddevcoder/activity/Main3Activity.java
@@ -0,0 +1,15 @@
+package com.allen.androiddevcoder.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.allen.androiddevcoder.R;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
